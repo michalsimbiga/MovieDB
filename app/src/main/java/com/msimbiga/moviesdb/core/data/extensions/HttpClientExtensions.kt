5723b8d36120ccab5234822fa4bc5e42a62188c2
TechnoViking <michal.simbiga@gmail.com>
2025-02-09 11:37:19 +0100

    Adds helper functions for HttpClient

---
+++ app/src/main/java/com/msimbiga/moviesdb/core/data/extensions/HttpClientExtensions.kt
@@ -0,0 +1,37 @@
+package com.msimbiga.moviesdb.core.data.extensions
+
+import com.msimbiga.moviesdb.core.domain.DataError
+import com.msimbiga.moviesdb.core.domain.Result
+import io.ktor.client.call.body
+import io.ktor.client.statement.HttpResponse
+import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.serialization.SerializationException
+import kotlin.coroutines.cancellation.CancellationException
+
+suspend inline fun <reified T> safeCall(execute: () -> HttpResponse): Result<T, DataError.Network> {
+    val response = try {
+        execute()
+    } catch (e: UnresolvedAddressException) {
+        e.printStackTrace()
+        return Result.Error(DataError.Network.NO_INTERNET)
+    } catch (e: SerializationException) {
+        e.printStackTrace()
+        return Result.Error(DataError.Network.SERIALIZATION)
+    } catch (e: Exception) {
+        if (e is CancellationException) throw e
+        e.printStackTrace()
+        return Result.Error(DataError.Network.UNKNOWN)
+    }
+
+    return responseToResult(response)
+}
+
+suspend inline fun <reified T> responseToResult(response: HttpResponse): Result<T, DataError.Network> {
+    return when (response.status.value) {
+        in 200..299 -> Result.Success(response.body<T>())
+        408 -> Result.Error(DataError.Network.REQUEST_TIMEOUT)
+        429 -> Result.Error(DataError.Network.TOO_MANY_REQUESTS)
+        in 500..599 -> Result.Error(DataError.Network.SERVER_ERROR)
+        else -> Result.Error(DataError.Network.UNKNOWN)
+    }
+}
