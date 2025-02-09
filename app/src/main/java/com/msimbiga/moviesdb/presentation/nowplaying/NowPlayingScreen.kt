eea465d1c1909dac27946ba106d07f98dd4f2e7e
TechnoViking <michal.simbiga@gmail.com>
2025-02-09 15:48:59 +0100

    Adds loading and error states handling

--- app/src/main/java/com/msimbiga/moviesdb/presentation/nowplaying/NowPlayingScreen.kt
+++ app/src/main/java/com/msimbiga/moviesdb/presentation/nowplaying/NowPlayingScreen.kt
@@ -1,19 +1,24 @@
 package com.msimbiga.moviesdb.presentation.nowplaying
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.msimbiga.moviesdb.core.presentation.ObserveAsEvents
+import com.msimbiga.moviesdb.presentation.components.DefaultErrorView
 import com.msimbiga.moviesdb.presentation.components.TopAppBar
 import com.msimbiga.moviesdb.presentation.nowplaying.components.MovieTile
 import kotlinx.serialization.Serializable
@@ -43,7 +48,7 @@ fun NowPlayingScreenRoot(
 @Composable
 @Preview
 fun NowPlayingScreenContent(
-    state: NowPlayingState = NowPlayingState(movies = emptyList(), isLoading = true),
+    state: NowPlayingState = NowPlayingState.Loading,
     onAction: (NowPlayingAction) -> Unit = {}
 ) {
     // TODO: Add Error and loading views
@@ -52,24 +57,37 @@ fun NowPlayingScreenContent(
         topBar = { TopAppBar(title = "Now playing") }
     ) { paddingValues ->
 
-        val scrollState = rememberLazyGridState()
+        when (state) {
+            is NowPlayingState.Error -> {
+                DefaultErrorView(
+                    onRetryClick = { onAction(NowPlayingAction.OnErrorRetryClicked) }
+                )
+            }
 
-        if (state.movies.isNotEmpty()) {
-            LazyVerticalGrid(
-                state = scrollState,
-                columns = GridCells.Adaptive(minSize = 180.dp),
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                contentPadding = paddingValues
-            ) {
-                items(state.movies, key = { movie -> movie.id }) { movie ->
-                    MovieTile(
-                        movie = movie,
-                        onClick = { onAction(NowPlayingAction.OnMovieSelected(movie.id)) }
-                    )
+            NowPlayingState.Loading -> {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    CircularProgressIndicator()
                 }
             }
 
-        }
+            is NowPlayingState.Success -> {
+                val scrollState = rememberLazyGridState()
 
+                LazyVerticalGrid(
+                    modifier = Modifier.padding(horizontal = 8.dp),
+                    state = scrollState,
+                    columns = GridCells.Adaptive(minSize = 168.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    contentPadding = paddingValues
+                ) {
+                    items(state.movies, key = { movie -> movie.id }) { movie ->
+                        MovieTile(
+                            movie = movie,
+                            onClick = { onAction(NowPlayingAction.OnMovieSelected(movie.id)) }
+                        )
+                    }
+                }
+            }
+        }
     }
 }
