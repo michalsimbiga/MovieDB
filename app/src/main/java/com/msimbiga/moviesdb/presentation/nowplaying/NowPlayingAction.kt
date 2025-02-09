ed7b8ce13e0a54d6bc33319bd46569771b9575b6
TechnoViking <michal.simbiga@gmail.com>
2025-02-09 15:47:49 +0100

    Adds OnErrorRetryClicked

--- app/src/main/java/com/msimbiga/moviesdb/presentation/nowplaying/NowPlayingAction.kt
+++ app/src/main/java/com/msimbiga/moviesdb/presentation/nowplaying/NowPlayingAction.kt
@@ -2,4 +2,5 @@ package com.msimbiga.moviesdb.presentation.nowplaying
 
 sealed interface NowPlayingAction {
     data class OnMovieSelected(val id: Int) : NowPlayingAction
+    data object OnErrorRetryClicked : NowPlayingAction
 }
