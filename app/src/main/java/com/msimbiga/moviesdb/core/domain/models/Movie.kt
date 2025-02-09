ac33d6ddd3213e12419937d0443de03aad82b0ca
TechnoViking <michal.simbiga@gmail.com>
2025-02-09 21:16:48 +0100

    Stripped Move model of unused properties

--- app/src/main/java/com/msimbiga/moviesdb/core/domain/models/Movie.kt
+++ app/src/main/java/com/msimbiga/moviesdb/core/domain/models/Movie.kt
@@ -2,37 +2,23 @@ package com.msimbiga.moviesdb.core.domain.models
 
 data class Movie(
     val id: Int,
-    val adult: Boolean,
     val backdropPath: String?,
-    val genreIds: List<Int>,
-    val originalLanguage: String,
-    val originalTitle: String,
     val overview: String,
-    val popularity: Double,
     val posterPath: String?,
     val releaseDate: String,
     val title: String,
-    val video: Boolean,
     val voteAverage: Double,
-    val voteCount: Int
 ) {
 
     companion object {
         val mock = Movie(
             id = 939243,
-            adult = false,
             backdropPath = "/zOpe0eHsq0A2NvNyBbtT6sj53qV.jpg",
-            genreIds = listOf(28, 878, 35, 10751),
-            originalLanguage = "en",
-            originalTitle = "Sonic the Hedgehog 3",
             overview = "Sonic, Knuckles, and Tails reunite against a powerful new adversary, Shadow, a mysterious villain with powers unlike anything they have faced before. With their abilities outmatched in every way, Team Sonic must seek out an unlikely alliance in hopes of stopping Shadow and protecting the planet.",
-            popularity = 4014.717,
             posterPath = "/d8Ryb8AunYAuycVKDp5HpdWPKgC.jpg",
             releaseDate = "2024-12-19",
             title = "Sonic the Hedgehog 3",
-            video = false,
             voteAverage = 7.776,
-            voteCount = 1610
         )
     }
 }
--- app/src/main/java/com/msimbiga/moviesdb/presentation/models/MovieItem.kt
+++ app/src/main/java/com/msimbiga/moviesdb/presentation/models/MovieItem.kt
@@ -6,17 +6,10 @@ import java.math.RoundingMode
 
 data class MovieItem(
     val id: Int,
-    val adult: Boolean,
-    val genreIds: List<Int>,
-    val originalLanguage: String,
-    val originalTitle: String,
     val overview: String,
-    val popularity: Double,
     val releaseDate: String,
     val title: String,
-    val video: Boolean,
     val voteAverage: Double,
-    val voteCount: Int,
     val posterUrl: String?,
     val posterPath: String?,
     val imageUrl: String?,
@@ -24,18 +17,11 @@ data class MovieItem(
 
 fun Movie.toUi() = MovieItem(
     id = id,
-    adult = adult,
     posterUrl = if (posterPath != null) "https://image.tmdb.org/t/p/original$posterPath" else null,
     imageUrl = if (backdropPath != null) "https://image.tmdb.org/t/p/original$backdropPath" else null,
-    genreIds = genreIds,
-    originalLanguage = originalLanguage,
-    originalTitle = originalTitle,
     overview = overview,
-    popularity = popularity,
     posterPath = posterPath,
     releaseDate = releaseDate.split("-").firstOrNull().orEmpty(),
     title = title,
-    video = video,
     voteAverage = BigDecimal(voteAverage).setScale(1, RoundingMode.HALF_EVEN).toDouble(),
-    voteCount = voteCount,
 )
