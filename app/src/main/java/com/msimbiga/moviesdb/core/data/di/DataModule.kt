2b5b13445c5a326c8067703ab079e0c79b77ca2b
TechnoViking <michal.simbiga@gmail.com>
2025-02-09 21:19:45 +0100

    Adds Di for MovieLocalDataSource

--- app/src/main/java/com/msimbiga/moviesdb/core/data/di/DataModule.kt
+++ app/src/main/java/com/msimbiga/moviesdb/core/data/di/DataModule.kt
@@ -1,10 +1,14 @@
 package com.msimbiga.moviesdb.core.data.di
 
+import android.content.Context
+import com.msimbiga.moviesdb.core.data.datasource.MoviesLocalDataSource
 import com.msimbiga.moviesdb.core.data.repository.MoviesRepositoryImpl
 import com.msimbiga.moviesdb.core.domain.repository.MoviesRepository
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -15,4 +19,12 @@ abstract class DataModule {
     @Binds
     @Singleton
     abstract fun bindMoviesRepository(impl: MoviesRepositoryImpl): MoviesRepository
+
+    companion object {
+
+        @Provides
+        @Singleton
+        fun provideMovieLocalDataSource(@ApplicationContext context: Context): MoviesLocalDataSource =
+            MoviesLocalDataSource(context)
+    }
 }
