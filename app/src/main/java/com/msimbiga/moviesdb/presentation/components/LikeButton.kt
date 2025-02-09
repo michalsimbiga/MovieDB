d2e427c9eeb55e38bc959064161600cb2c9f3906
TechnoViking <michal.simbiga@gmail.com>
2025-02-09 21:03:46 +0100

    Adds like button

---
+++ app/src/main/java/com/msimbiga/moviesdb/presentation/components/LikeButton.kt
@@ -0,0 +1,75 @@
+package com.msimbiga.moviesdb.presentation.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun LikeButton(
+    modifier: Modifier = Modifier,
+    liked: Boolean = false,
+    onLikeClicked: () -> Unit = {},
+    hasBackground: Boolean = true,
+) {
+    val (enter, exit) = remember {
+        arrayListOf(
+            fadeIn(animationSpec = tween(durationMillis = 1500)),
+            fadeOut(animationSpec = tween(durationMillis = 500))
+        )
+    }
+
+    IconButton(
+        onClick = onLikeClicked,
+        modifier = modifier
+            .size(40.dp)
+            .apply {
+                if (hasBackground) {
+                    background(MaterialTheme.colorScheme.onBackground)
+                }
+            }
+    ) {
+        AnimatedVisibility(
+            visible = !liked,
+            enter = enter as EnterTransition,
+            exit = exit as ExitTransition
+        ) {
+            Image(
+                imageVector = Icons.Default.FavoriteBorder,
+                contentDescription = null,
+                modifier = Modifier
+            )
+        }
+
+        AnimatedVisibility(
+            visible = liked,
+            enter = enter,
+            exit = exit
+        ) {
+            Image(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = null,
+                modifier = Modifier,
+                colorFilter = ColorFilter.tint(Color.Red)
+            )
+        }
+    }
+}
