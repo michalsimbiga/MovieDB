eaafa029553716bba579f0c39ab8a2d2572cdb77
TechnoViking <michal.simbiga@gmail.com>
2025-02-09 21:05:10 +0100

    Adds setMovieLiked and getLikedMoviesFlow

--- app/src/main/java/com/msimbiga/moviesdb/core/data/repository/MoviesRepositoryImpl.kt
+++ app/src/main/java/com/msimbiga/moviesdb/core/data/repository/MoviesRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.msimbiga.moviesdb.core.data.repository
 
+import com.msimbiga.moviesdb.core.data.datasource.MoviesLocalDataSource
 import com.msimbiga.moviesdb.core.data.datasource.MoviesNetworkDataSource
 import com.msimbiga.moviesdb.core.data.models.MovieDetailsDTO
 import com.msimbiga.moviesdb.core.data.models.NowPlayingPageDTO
@@ -11,11 +12,13 @@ import com.msimbiga.moviesdb.core.domain.models.MovieDetails
 import com.msimbiga.moviesdb.core.domain.models.NowPlayingPage
 import com.msimbiga.moviesdb.core.domain.models.SearchPage
 import com.msimbiga.moviesdb.core.domain.repository.MoviesRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 
 class MoviesRepositoryImpl @Inject constructor(
-    private val moviesNetworkDataSource: MoviesNetworkDataSource
+    private val moviesNetworkDataSource: MoviesNetworkDataSource,
+    private val moviesLocalDataSource: MoviesLocalDataSource
 ) : MoviesRepository {
 
     override suspend fun fetchNowPlayingPage(page: Int?): Result<NowPlayingPage, Error> =
@@ -35,5 +38,17 @@ class MoviesRepositoryImpl @Inject constructor(
         moviesNetworkDataSource
             .getMovieDetails(id)
             .map(MovieDetailsDTO::toDomain)
+
+    override suspend fun setMovieLiked(id: Int, isLiked: Boolean): Result<Any, Error> {
+        if (isLiked) {
+            moviesLocalDataSource.saveMovieId(id)
+        } else {
+            moviesLocalDataSource.removeMovieId(id)
+        }
+        return Result.Success(Unit)
+    }
+
+    override fun getLikedMoviesFlow(): Flow<List<Int>> =
+        moviesLocalDataSource.getLikedMoviesFlow()
 
 }
--- app/src/main/java/com/msimbiga/moviesdb/core/domain/repository/MoviesRepository.kt
+++ app/src/main/java/com/msimbiga/moviesdb/core/domain/repository/MoviesRepository.kt
@@ -5,9 +5,13 @@ import com.msimbiga.moviesdb.core.domain.Result
 import com.msimbiga.moviesdb.core.domain.models.MovieDetails
 import com.msimbiga.moviesdb.core.domain.models.NowPlayingPage
 import com.msimbiga.moviesdb.core.domain.models.SearchPage
+import kotlinx.coroutines.flow.Flow
 
 interface MoviesRepository {
     suspend fun fetchNowPlayingPage(page: Int?): Result<NowPlayingPage, Error>
     suspend fun fetchSearchPage(searchTerm: String, page: Int?): Result<SearchPage, Error>
     suspend fun getMovieDetails(id: Int): Result<MovieDetails, Error>
+
+    suspend fun setMovieLiked(id: Int, isLiked: Boolean): Result<Any, Error>
+    fun getLikedMoviesFlow(): Flow<List<Int>>
 }
