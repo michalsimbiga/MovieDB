0055eba739d4407ef09f3bb6a6bf2c695caf2c85
TechnoViking <michal.simbiga@gmail.com>
2025-02-09 15:06:43 +0100

    Adds lazy vertical grid with adaptive colums

--- app/src/main/java/com/msimbiga/moviesdb/presentation/nowplaying/NowPlayingScreen.kt
+++ app/src/main/java/com/msimbiga/moviesdb/presentation/nowplaying/NowPlayingScreen.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -11,6 +14,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.msimbiga.moviesdb.core.presentation.ObserveAsEvents
+import com.msimbiga.moviesdb.presentation.components.TopAppBar
 import com.msimbiga.moviesdb.presentation.nowplaying.components.MovieTile
 import kotlinx.serialization.Serializable
 
@@ -44,19 +48,28 @@ fun NowPlayingScreenContent(
 ) {
     // TODO: Add Error and loading views
 
-    if (state.movies.isNotEmpty()) {
-        LazyVerticalGrid(
-            columns = GridCells.Adaptive(minSize = 180.dp),
-            horizontalArrangement = Arrangement.spacedBy(4.dp)
-        ) {
-            items(state.movies) { movie ->
-                MovieTile(
-                    movie = movie,
-                    onClick = { onAction(NowPlayingAction.OnMovieSelected(movie.id)) }
-                )
+    Scaffold(
+        topBar = { TopAppBar(title = "Now playing") }
+    ) { paddingValues ->
+
+        val scrollState = rememberLazyGridState()
+
+        if (state.movies.isNotEmpty()) {
+            LazyVerticalGrid(
+                state = scrollState,
+                columns = GridCells.Adaptive(minSize = 180.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                contentPadding = paddingValues
+            ) {
+                items(state.movies, key = { movie -> movie.id }) { movie ->
+                    MovieTile(
+                        movie = movie,
+                        onClick = { onAction(NowPlayingAction.OnMovieSelected(movie.id)) }
+                    )
+                }
             }
+
         }
 
     }
-
 }
