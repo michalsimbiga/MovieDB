f1d85ec59c52a54f98d206276f274c34b95ab0f2
TechnoViking <michal.simbiga@gmail.com>
2025-02-09 19:46:14 +0100

    Implement search mechanism

--- app/src/main/java/com/msimbiga/moviesdb/presentation/search/SearchViewModel.kt
+++ app/src/main/java/com/msimbiga/moviesdb/presentation/search/SearchViewModel.kt
@@ -1,28 +1,89 @@
 package com.msimbiga.moviesdb.presentation.search
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.msimbiga.moviesdb.core.domain.Result
+import com.msimbiga.moviesdb.core.domain.models.Movie
+import com.msimbiga.moviesdb.core.domain.repository.MoviesRepository
+import com.msimbiga.moviesdb.presentation.models.toUi
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class SearchViewModel : ViewModel() {
+class SearchViewModel @Inject constructor(
+    private val moviesRepository: MoviesRepository
+) : ViewModel() {
+
+    private val _event = Channel<SearchEvent>()
+    val event = _event.receiveAsFlow()
 
     private val _state = MutableStateFlow<SearchState>(
         SearchState(
-            isLoading = true,
+            isLoading = false,
             searchTerm = "",
             suggestions = emptyList()
         )
     )
-    val state = _state.asStateFlow()
+    val state = _state
+        .onStart { observeTextChanges() }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000L),
+            initialValue = _state.value
+        )
 
     fun onAction(action: SearchAction) {
         when (action) {
             is SearchAction.SearchTermUpdated -> {
                 _state.update { it.copy(searchTerm = action.searchTerm) }
             }
+
+            is SearchAction.OnMovieClicked -> {
+                _event.trySend(SearchEvent.OnMovieSelected(action.id))
+            }
         }
     }
+
+    private fun observeTextChanges() {
+        viewModelScope.launch(Dispatchers.IO) {
+            state
+                .distinctUntilChanged { old, new -> old.searchTerm == new.searchTerm }
+                .map { it.searchTerm }
+                .debounce(1000L)
+                .collect { searchTerm ->
+                    if (searchTerm.isEmpty()) return@collect
+                    _state.update { it.copy(isLoading = true) }
+                    Log.d("VUKO", "should search for term $searchTerm")
+
+                    when (val results = moviesRepository.fetchSearchPage(searchTerm, null)) {
+                        is Result.Error -> {
+                            _state.update { it.copy(isLoading = false) }
+                        }
+
+                        is Result.Success -> {
+                            _state.update {
+                                it.copy(
+                                    suggestions = results.data.results.map(Movie::toUi),
+                                    isLoading = false
+                                )
+                            }
+                        }
+                    }
+                }
+        }
+    }
+
 }
