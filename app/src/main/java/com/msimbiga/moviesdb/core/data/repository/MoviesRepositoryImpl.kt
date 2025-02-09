9a3170211e7926b4d8df8cb2cc58940380ada149
TechnoViking <michal.simbiga@gmail.com>
2025-02-09 19:46:55 +0100

    Adds fetchSearchPage and its implementation

--- app/src/main/java/com/msimbiga/moviesdb/core/data/repository/MoviesRepositoryImpl.kt
+++ app/src/main/java/com/msimbiga/moviesdb/core/data/repository/MoviesRepositoryImpl.kt
@@ -3,11 +3,13 @@ package com.msimbiga.moviesdb.core.data.repository
 import com.msimbiga.moviesdb.core.data.datasource.MoviesNetworkDataSource
 import com.msimbiga.moviesdb.core.data.models.MovieDetailsDTO
 import com.msimbiga.moviesdb.core.data.models.NowPlayingPageDTO
+import com.msimbiga.moviesdb.core.data.models.SearchPageDTO
 import com.msimbiga.moviesdb.core.domain.Error
 import com.msimbiga.moviesdb.core.domain.Result
 import com.msimbiga.moviesdb.core.domain.map
 import com.msimbiga.moviesdb.core.domain.models.MovieDetails
 import com.msimbiga.moviesdb.core.domain.models.NowPlayingPage
+import com.msimbiga.moviesdb.core.domain.models.SearchPage
 import com.msimbiga.moviesdb.core.domain.repository.MoviesRepository
 import javax.inject.Inject
 
@@ -20,6 +22,14 @@ class MoviesRepositoryImpl @Inject constructor(
         moviesNetworkDataSource
             .getNowPlaying()
             .map(NowPlayingPageDTO::toDomain)
+
+    override suspend fun fetchSearchPage(
+        searchTerm: String,
+        page: Int?
+    ): Result<SearchPage, Error> =
+        moviesNetworkDataSource
+            .getSearchSuggestions(searchTerm)
+            .map(SearchPageDTO::toDomain)
 
     override suspend fun getMovieDetails(id: Int): Result<MovieDetails, Error> =
         moviesNetworkDataSource
--- app/src/main/java/com/msimbiga/moviesdb/core/domain/repository/MoviesRepository.kt
+++ app/src/main/java/com/msimbiga/moviesdb/core/domain/repository/MoviesRepository.kt
@@ -4,11 +4,10 @@ import com.msimbiga.moviesdb.core.domain.Error
 import com.msimbiga.moviesdb.core.domain.Result
 import com.msimbiga.moviesdb.core.domain.models.MovieDetails
 import com.msimbiga.moviesdb.core.domain.models.NowPlayingPage
+import com.msimbiga.moviesdb.core.domain.models.SearchPage
 
 interface MoviesRepository {
-
     suspend fun fetchNowPlayingPage(page: Int?): Result<NowPlayingPage, Error>
-
+    suspend fun fetchSearchPage(searchTerm: String, page: Int?): Result<SearchPage, Error>
     suspend fun getMovieDetails(id: Int): Result<MovieDetails, Error>
-
 }
