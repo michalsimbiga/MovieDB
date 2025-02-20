a838746fdf77b0691bfc0401f4e463b4247e40ad
michalsimbiga <michal.simbiga@gmail.com>
2025-02-21 00:43:47 +0100

    Changes NowPlayingState to be of sealed interface type

--- app/src/main/java/com/msimbiga/moviesdb/presentation/nowplaying/NowPlayingState.kt
+++ app/src/main/java/com/msimbiga/moviesdb/presentation/nowplaying/NowPlayingState.kt
@@ -1,14 +1,15 @@
 package com.msimbiga.moviesdb.presentation.nowplaying
 
 import androidx.compose.runtime.Immutable
+import androidx.paging.PagingData
 import com.msimbiga.moviesdb.presentation.models.MovieItem
 
 @Immutable
-data class NowPlayingState(
-    val isLoading: Boolean = false,
-    val isError: Boolean = false,
-    val movies: List<MovieItem> = emptyList(),
-    val page: Int = 0,
-    val hasMore: Boolean = false
-)
+sealed interface NowPlayingState {
+    data object Loading : NowPlayingState
+    data object Error : NowPlayingState
+    data class Success(
+        val moviesPagingData: PagingData<MovieItem> = PagingData.empty()
+    ) : NowPlayingState
+}
 
