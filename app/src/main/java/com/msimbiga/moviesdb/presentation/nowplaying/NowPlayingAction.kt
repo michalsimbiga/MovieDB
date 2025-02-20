cb7bcca4bb17e87f37f68497f8e47878bd26744d
michalsimbiga <michal.simbiga@gmail.com>
2025-02-21 00:44:00 +0100

    Removes unused actions

--- app/src/main/java/com/msimbiga/moviesdb/presentation/nowplaying/NowPlayingAction.kt
+++ app/src/main/java/com/msimbiga/moviesdb/presentation/nowplaying/NowPlayingAction.kt
@@ -3,5 +3,4 @@ package com.msimbiga.moviesdb.presentation.nowplaying
 sealed interface NowPlayingAction {
     data class OnMovieSelected(val id: Int) : NowPlayingAction
     data object OnErrorRetryClicked : NowPlayingAction
-    data object OnGetNextPage : NowPlayingAction
 }
